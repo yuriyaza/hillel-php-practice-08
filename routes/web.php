bfe43f2a56180f75206b86a21671f525f04b1085
yuriyaza <yuriy.aza@gmail.com>
2024-02-19 13:52:16 +0200

    Added incomming parameters validation

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use App\Http\Controllers\MinimizeUrlController;
 use App\Http\Controllers\RestoreUrlController;
+use App\Http\Middleware\MinimizeUrlValidation;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/minimize', [MinimizeUrlController::class, 'index']);
+Route::get('/minimize', [MinimizeUrlController::class, 'index'])
+    ->middleware([MinimizeUrlValidation::class]);
+
 Route::get('/{shortUrl}', [RestoreUrlController::class, 'index']);
